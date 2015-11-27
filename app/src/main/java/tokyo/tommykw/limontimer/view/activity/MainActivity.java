fd0d1ac8d966f34ede1cc9cab84021c94ada38bd
kenji tomita <destinys.lotus.1108mn@gmail.com>
2015-11-27 21:03:10 +0900

    Add actionbar

--- app/src/main/java/tokyo/tommykw/limontimer/view/activity/MainActivity.java
+++ app/src/main/java/tokyo/tommykw/limontimer/view/activity/MainActivity.java
@@ -5,6 +5,9 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.balysv.materialripple.MaterialRippleLayout;
@@ -31,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         activityBinding.timerButton.setOnClickListener(onClickTimerListener);
         timerPresenter = getTimerPresenter();
         snackbar = Snackbar.make(activityBinding.coordinatorLayout, null, Snackbar.LENGTH_LONG);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         MaterialRippleLayout.on(activityBinding.rippleTimerButtonLayout)
                 .rippleColor(Color.parseColor("#FF0000"))
@@ -77,5 +83,27 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         return presenter;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
